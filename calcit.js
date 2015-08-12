382c2d6504a5659c98efd7da2e87fe2ecd471529
Ambar Sethi <ambarrrsethi@gmail.com>
2015-08-12 18:20:13 +0100

    Calc layout inplace trying to get answer

---
+++ calcit.js
@@ -0,0 +1,50 @@
+
+/*Basic Calculator
+
+Must be able to +, -, / and *
+Must take user input using prompt
+Must be able display the result using alert
+Should use a loop!
+Should handle floats*/
+
+
+var op =  prompt('choose an operation');
+console.log('op', op);
+
+var num1 = prompt('choose a number');
+console.log('num1', num1);
+
+var num2 = prompt('choose another number');
+console.log('num1', num2);
+
+num1 = parseInt(num1);
+num2= parseInt(num2);
+
+
+var answer
+
+alert("Your answer is: " + answer);
+console.log('answer', answer);
+
+
+
+
+
+
+if (op === '+'){
+    
+    answer= num1 + num2;
+
+  } else if (op === '-' ){
+    
+    answer= num1 - num2;
+
+  } else if (op === '/' ){
+    
+    answer= num1/num2;
+
+  } else if (op === '*' ){
+    
+    answer= num1 * num2;
+
+  }
